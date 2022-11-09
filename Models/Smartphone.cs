9a7c1ed977d3f66adc0d605c5cf26b2bed4a8007
Aryel Sander dos Santos <aryelsander@live.com>
2022-11-09 18:43:37 -0300

    add polymorphism

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,16 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero,string modelo,string imei,int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -19,6 +23,10 @@ namespace DesafioPOO.Models
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+        }
+        public override string ToString()
+        {
+            return $"Numero: {Numero}\nModelo: {Modelo}\nIMEI: {IMEI}\nMemoria: {Memoria}";
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("1","Lumia","1243213",1024);
+Iphone iphone = new Iphone("2","Iphone 6","414124214",1024);
+
+
+System.Console.WriteLine("Teste Nokia");
+System.Console.WriteLine(nokia.ToString());
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+System.Console.WriteLine();
+
+System.Console.WriteLine("Teste Iphone");
+System.Console.WriteLine(iphone.ToString());
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
